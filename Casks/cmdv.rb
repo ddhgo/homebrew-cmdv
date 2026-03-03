5396877fcbe2fd459fc906037d1aef0d4a0f8eaf
rtfdev <kttu0008@gmail.com>
2026-03-04 00:01:13 +0900

    Update cmdv to 0.1.3

--- Casks/cmdv.rb
+++ Casks/cmdv.rb
@@ -1,6 +1,6 @@
 cask "cmdv" do
-  version "0.1.2"
-  sha256 "3f20768ae50afb0872e02065d83dcc39b2daaafc2ab6cf0a90797e8facf5c453"
+  version "0.1.3"
+  sha256 "0628d3aec0dcb1475df42999c8332b85c9ed123f84a9d1c66c5f22a0380781d8"
 
   url "https://github.com/ddhgo/CmdV/releases/download/v#{version}/CmdV-v#{version}.zip"
   name "CmdV"
