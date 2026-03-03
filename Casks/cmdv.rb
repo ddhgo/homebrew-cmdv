53e202429892d7408f9529a2bbdbf5d34959f2f6
ddhgo <ddhgo.dev@gmail.com>
2026-03-03 23:05:32 +0900

    fix: remove quarantine flag after install for unsigned app

--- Casks/cmdv.rb
+++ Casks/cmdv.rb
@@ -11,6 +11,12 @@ cask "cmdv" do
 
   app "CmdV.app"
 
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-dr", "com.apple.quarantine", "#{appdir}/CmdV.app"],
+                   sudo: false
+  end
+
   zap trash: [
     "~/Library/Application Support/CmdV",
     "~/Library/Preferences/com.ddhgo.CmdV.plist",
